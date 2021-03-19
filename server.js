bee939b4d32ff836edbbc45bb1c941c17a663bcf
Borris FONTAINE <borris.fontaine@outlook.com>
2021-03-19 10:36:05 +0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
+const secret = "sz,sopfiION45OIF5DNn6nNIUJIByUyÈ_Ç_hhÈT--Èè-((U6azaa454f4eIIKNO4545dsz";
+
+
+const userTest = {email:'test@email.com', password : '1234'};
+
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+app.use(bodyParser.json());
+
+const api = express.Router();
+const auth = express.Router();
+
+auth.post('/login', (req,res)=> {   
+    if (req.body && req.body.email && req.body.password) {     
+        const email = req.body.email.toLocaleLowerCase();
+        const password = req.body.password;
+
+        if(userTest.email === email && userTest.password === password) {
+            console.log('credentials ok')
+            delete req.body.password;
+
+            const token = jwt.sign({ iss:'http://localhost:5000', role:'customer'}, secret);
+            
+            res.json({success:true, data: req.body, token});
+            
+        }else {
+            console.log('credentials KO')
+            res.json({success:false, message:"invalid credentials"});
+        }
+    }else {
+        res.json({success:true, message:'missing datas'})
+    }
+})
+
+auth.get('/restaurants', (req,res)=> {
+    res.json({success: true, data : [{id : 1, name: 'Restaurant 1'}, {id : 2, name: 'Restaurant 2'} ]});
+
+})
+
+api.get('/restaurants', (req,res)=> {
+    res.json({success: true, data : [{id : 1, name: 'Restaurant 1'}, {id : 2, name: 'Restaurant 2'} ]});
+
+})
+
+app.use('/api', api);
+app.use('/auth', auth);
+
+const port = 5000;
+
+app.listen(port, ()=> {
+    console.log( `listening on port ${port}`);
+} )
