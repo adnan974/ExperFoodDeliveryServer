396c2cd4cfb3437df687d94d4e407f0622e413c0
Borris FONTAINE <borris.fontaine@outlook.com>
2021-03-25 08:26:00 +0400

    Add register feature

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,5 @@
 module.exports = (router) => {    
-    router.use("/auth", require("./auth"));
+    router.use("/", require("./auth"));
     router.use("/users", require("./user"));
     router.use("/restaurants", require("./restaurant"));
     // router.use("/menus", require("./menu"));
